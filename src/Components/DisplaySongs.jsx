b80867fdfe813cf7a1d2a7776dbed9e3db27372a
rabialhosen404 <rabialhosen404@gmail.com>
2023-09-02 12:11:06 +0530

    l

--- src/Components/DisplaySongs.jsx
+++ src/Components/DisplaySongs.jsx
@@ -1,22 +1,38 @@
-import React from 'react'
-import songData from "../data/songData"
+import React from 'react';
+import songData from "../data/songData";
 import { v4 as uuidv4 } from "uuid";
 import { songsAction } from '../redux/actions/songsAction';
-import {useDispatch, useSelector } from 'react-redux';
-const DisplaySongs = () => {
+import { useDispatch, useSelector } from 'react-redux';
 
-  const dispatch = useDispatch();
+
+const DisplaySongs = () => {
+  
+
+
+
+
+  const dispatch = useDispatch()
+  const songs = useSelector((state) => state.songs);
 
 
   return (
     <div>
       <h1>List Of Songs Are :-----</h1>
       {
-        songData.map((song, index) => (
+        songData.map((song) =>
+          
+        (
+
+
           <div key={uuidv4()}>
-            <h3 onClick={()=>dispatch(songsAction(song))}>{song.name}</h3>
+
+            <h3 onClick={() => dispatch(songsAction(song))}>{song.name}</h3>
+            
           </div>
+
         ))
+
+
       }
     </div>
   )
